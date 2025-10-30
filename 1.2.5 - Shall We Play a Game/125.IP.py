452e76c5e08f190276429e7db01b544726f4d0f9
Ilya-CSP <316886@shakopeeschools.org>
2025-10-30 09:52:05 -0500

    Update 125.IP.py
    Log button

--- 1.2.5 - Shall We Play a Game/125.IP.py
+++ 1.2.5 - Shall We Play a Game/125.IP.py
@@ -26,6 +26,10 @@ for i in range(len(tictactoe_list)):
     main_menu_text.write(tictactoe_list[i], font=font_setup_title, align="center")
     title_distance+=138
 
+#defines which part of the game is currently on the screen
+main_menu = True #defines if it is the main menu on the screen
+game_going = False #is the game going?
+
 #match log button
 matchlog_button = trtl.Turtle()
 matchlog_button.hideturtle()
@@ -36,6 +40,22 @@ matchlog_button.penup()
 
 matchlog_button.goto(0,-75)
 matchlog_button.write("Match log",font=main_menu_font,align="center")
+
+log_button_x_min = -80
+log_button_x_max = 80
+log_button_y_min = -105
+log_button_y_max = -40
+
+match_log = False
+
+def log_click(x,y):
+    if log_button_x_max >= x >= log_button_x_min and log_button_y_max >= y >= log_button_y_min:
+        global main_menu
+        if main_menu == True:
+            wn.clear()
+            global match_log
+            main_menu = False
+            match_log = True
 #pull out the names and scores
 
 #write them in order
@@ -53,8 +73,6 @@ play_button.goto(0,25)
 play_button.write("Play", font=main_menu_font, align="center")
 play_button.color("white")
 
-main_menu = True #defines if it is a main menu on the screen
-
 play_button_x_min = -40
 play_button_x_max = 40
 play_button_y_min = 20
@@ -64,7 +82,7 @@ play_button_y_max = 55
 player_name1 = ""
 player_name2 = ""
 
-first_turn_rule = "n"
+first_turn_rule = "n" #default
 
 def play_click(x,y):
     if play_button_x_max >= x >= play_button_x_min and play_button_y_max >= y >= play_button_y_min:
@@ -74,32 +92,40 @@ def play_click(x,y):
             global player_name1
             global player_name2
             global first_turn_rule
+            global game_going
             player_name1 = trtl.textinput("Player 1", "Your name is...")
             player_name2 = trtl.textinput("Player 2", "Your name is...")
-            first_turn_rule = trtl.textinput("Each new round, the one who lost goes first(y/n)?")
+            first_turn_rule = trtl.textinput("Rule","Each new round, the one who lost goes first(y/n)?")
             main_menu = False
-
-wn.onscreenclick(play_click)
-
+            game_going = True
 
 #game board
 board_draw = trtl.Turtle()
 board_draw.speed("fastest")
 board_draw.hideturtle()
-line_draw = -150
-
-game = True
+line_y = -150 #starting position of vertical lines
+lines_y = 0 #number of vertical lines
 
 def game_board():
-    if game == True:
-        for i in range(2):
-            global line_draw
+    if game_going == True:
+        global line_y
+        global lines_y
+        while lines_y < 2:
             board_draw.penup()
-            board_draw.goto(line_draw,300)
+            board_draw.goto(line_y,300)
             board_draw.pendown()
-            board_draw.goto(line_draw,-300)
-game_board()
+            board_draw.goto(line_y,-300)
+            line_y = line_y + 300
+            lines_y = lines_y + 1
+            print("loop done", lines_y)
 
+
+def game_events(x,y):
+    play_click(x,y)
+    log_click(x,y)
+    game_board()
+
+wn.onscreenclick(game_events)
 #player names + scores
 
 #turn
