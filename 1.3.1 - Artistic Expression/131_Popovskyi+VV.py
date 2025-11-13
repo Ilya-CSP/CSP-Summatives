861b2fd5a3f31d38ad1e39ea50c3f1fada0ce9c3
Ilya-CSP <316886@shakopeeschools.org>
2025-11-13 09:44:24 -0600

    Update 131_Popovskyi+VV.py

--- 1.3.1 - Artistic Expression/131_Popovskyi+VV.py
+++ 1.3.1 - Artistic Expression/131_Popovskyi+VV.py
@@ -7,7 +7,9 @@ wn.bgcolor("blue")
 
 #MILESTONE 1: Ask name and age
 name = trtl.textinput("Your name", "What is your name?")
+name = str(name)
 age = trtl.textinput("Your age", "How old are you (num only)?")
+font_letter = "Arial", 15, "bold"
 
 #wn.addshape("square++", (-12,12), (-12,-12), (12,-12), (12,12))
 
@@ -64,9 +66,74 @@ if letter_open == "y":
     letter_draw.begin_fill()
 
     letter = trtl.Turtle()
+    letter.penup()
+    letter.speed(1)
     letter.shape("square")
-    letter.shapesize(10)
+    letter.shapesize(13)
     letter.color("white")
+    letter.goto(0,150)
 
+    letter_writer = trtl.Turtle()
+    letter_writer.speed("fastest")
+    letter_writer.hideturtle()
+    letter_writer.penup()
+    letter_writer.goto(0,180)
+    letter_writer.write("Dear " + name + ",\n we hope you are doing well,\n here is a fun game to play.", font=font_letter, align="center")
+
+    play_button = trtl.Turtle()
+    play_button.speed("fastest")
+    play_button.penup()
+    play_button.goto(-55,35)
+    play_button.write("Play", font=font_letter, align="center")
+    play_button.goto(-55,75)
+    play_button.shape("square")
+    play_button.shapesize(2)
+
+    close_button = trtl.Turtle()
+    close_button.speed("fastest")
+    close_button.penup()
+    close_button.goto(55,35)
+    close_button.write("Close", font=font_letter, align="center")
+    close_button.goto(55,75)
+    close_button.shape("square")
+    close_button.shapesize(2)
+    #  game functions
+
+words_list4 = ["BIKE", "BLUE", "RIDE" "FROG", "BALL", "SNOW", "HAND", "FOOT", "FACE", "BIRD", "RAIN", "WORD"]
+words_list5 =["APPLE","CHAIR","BREAD","PLANT","LIGHT","HEART","SMILE","TRUCK","TABLE","HOUSE","GRASS","SOLAR"]
+words_list6 = ["ANIMAL","BANANA","FLOWER","SCHOOL","FRIEND","PLANET","FOREST","FAMILY","ORANGE","RABBIT","SUMMER"]
+
+game_board = trtl.Turtle()
+x = -200
+y = -200
+def columns(n):
+    global game_board, x, y
+    for i in range(n):
+        game_board.goto(x,y)
+        game_
+
+
+
+'''def game_starter():
+    if 3<=age<6:
+        
+    elif 6<age<10:
+        ###
+    else:
+        ###'''
+
+
+
+def onclick_playbutton(x,y):
+    wn.clear()
+    print(x,y)
+    
+
+def onclick_closebutton(x,y):
+    print(x,y)
+    trtl.bye()
+
+play_button.onclick(onclick_playbutton)
+close_button.onclick(onclick_closebutton)
 wn.mainloop()
         
