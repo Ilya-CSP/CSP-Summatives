60eea56564b12a0e86e5f245df7a6cfd9216d70f
Ilya-CSP <316886@shakopeeschools.org>
2025-11-14 09:44:22 -0600

    Update 131_Popovskyi+VV.py
    Finished the game board, game in progress

--- 1.3.1 - Artistic Expression/131_Popovskyi+VV.py
+++ 1.3.1 - Artistic Expression/131_Popovskyi+VV.py
@@ -9,7 +9,9 @@ wn.bgcolor("blue")
 name = trtl.textinput("Your name", "What is your name?")
 name = str(name)
 age = trtl.textinput("Your age", "How old are you (num only)?")
+age = int(age)
 font_letter = "Arial", 15, "bold"
+font_writer = "Arial", 30, "bold"
 
 #wn.addshape("square++", (-12,12), (-12,-12), (12,-12), (12,12))
 
@@ -104,30 +106,74 @@ words_list5 =["APPLE","CHAIR","BREAD","PLANT","LIGHT","HEART","SMILE","TRUCK","T
 words_list6 = ["ANIMAL","BANANA","FLOWER","SCHOOL","FRIEND","PLANET","FOREST","FAMILY","ORANGE","RABBIT","SUMMER"]
 
 game_board = trtl.Turtle()
-x = -200
-y = -200
-def columns(n):
-    global game_board, x, y
-    for i in range(n):
-        game_board.goto(x,y)
-        game_
+game_board.speed("fastest")
+game_board.penup()
 
 
+#additional columns calculator
+if age <=6:
+    n = 0
+    ny = 0
+elif 7 <= age <=9:
+    n = 1
+    ny = 1
+elif 10 <= age:
+    n = 2
+    ny = 1
 
-'''def game_starter():
-    if 3<=age<6:
-        
-    elif 6<age<10:
-        ###
-    else:
-        ###'''
+#horizontal lines
+hx = -200-25*n 
+hx2 = 200+25*n
+hy = -350
 
+if 7 <= age <=9:
+    hx = -225
+    hx2 = 275
+
+#vertical lines
+vy = 250
+vy2 = -250
+vx =-200-25*n
+#####DONE
+def columns():
+    global game_board, hx, hy, n, vx, vy, vy2
+    for i in range(7):
+        game_board.goto(hx,hy)
+        print(hx,hy, hx2)
+        game_board.pendown()
+        game_board.goto(hx2,hy)
+        game_board.penup()
+        hy=hy+100
+    for i in range(5+ny):
+        game_board.goto(vx,vy)
+        game_board.pendown()
+        game_board.goto(vx,vy2)
+        game_board.penup()
+        vx=vx+100
+
+#determine word
+if age <=6:
+    i = randit.randint(0,11)
+    word = words_list4[i]
+elif 7 <= age <=9:
+    i = randit.randint(0,11)
+    word = words_list5[i]
+elif 10 <= age:
+    i = randit.randint(0,10)
+    word = words_list6[i]
+
+trtl_writer = trtl.Turtle()
+trtl_writer.hideturtle()
+
+
+def game_start():
+    
 
 
 def onclick_playbutton(x,y):
     wn.clear()
     print(x,y)
-    
+    columns()
 
 def onclick_closebutton(x,y):
     print(x,y)
